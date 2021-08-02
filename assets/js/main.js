254677277b55c11c56bacea5d1dfae55cba49ef3
unknown <promaswarna@gmail.com>
2021-08-03 01:05:02 +0600

    update

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(".testimonials").owlCarousel({
+    margin: 30,
+    loop: 'true',
+    autoplay: 'true',
+    autoplayTimeout: 5000,
+    center: 'true',
+    items: 1,
+    dots: 'true'
+});
+$(".specialists").owlCarousel({
+    margin: 20,
+    loop: 'true',
+    autoplay: 'true',
+    autoplayTimeout: 5000,
+    items: 4,
+    dots: 'true',
+    nav: 'true',
+    navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>']
+});
