3aa3274ecec3b47e5d5efc55792aa2a1e6700017
unknown <promaswarna@gmail.com>
2021-08-04 00:53:09 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,29 @@
 $(".testimonials").owlCarousel({
     margin: 30,
     loop: 'true',
-    autoplay: 'true',
+    autoplay: 'false',
     autoplayTimeout: 5000,
     center: 'true',
     items: 1,
-    dots: 'true'
+    dots: 'true',
+    nav: 'true',
 });
 $(".specialists").owlCarousel({
     margin: 20,
     loop: 'true',
-    autoplay: 'true',
-    autoplayTimeout: 5000,
     items: 4,
     dots: 'true',
     nav: 'true',
     navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>']
 });
+$(".sponsor").owlCarousel({
+    margin: 30,
+    loop: 'true',
+    items: 5,
+    autoplay: 'false',
+    autoplayTimeout: 5000,
+    dots: 'true',
+    nav: 'true',
+    navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>']
+});
+
